85e72e227665cfdf62942c393938b502d8280e57
Joel <joel.mckechnie@hotmail.com>
2020-06-02 21:51:40 +1000

    added several type examples

---
+++ printandinput/inputfun.py
@@ -0,0 +1,9 @@
+s = input("input your name:")
+print(s)
+
+i = int(input("input integer:"))
+print(i)
+print(type(i))
+
+lst = [int(x) for x in input("enter three numbers separated by comma:").split(",")]
+print(lst)
