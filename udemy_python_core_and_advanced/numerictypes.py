f592558c6b89f8fd77137a02427900d4229f8d0b
Joel <joel.mckechnie@hotmail.com>
2020-06-02 20:34:00 +1000

    added numerictypes.py

---
+++ udemy_python_core_and_advanced/numerictypes.py
@@ -0,0 +1,39 @@
+a = 13
+b = 100
+c = -66
+print(a, b, c)
+
+x = 33.5
+y = -25.8
+z = 205.0
+print(x, y, z)
+print(type(z))
+
+d = 3+5j
+print(d)
+print(type(d))
+
+e = 0B1010
+print(e)
+print(type(e))
+
+f = 0Xff
+print(f)
+print(type(f))
+
+g = True
+print(type(g))
+print(9 < 8)
+
+h = int(x)
+print(h)
+print(type(h))
+
+i = float("22.5")
+print(i)
+print(type(i))
+
+o = oct(10)
+print(o)
+print(type(o))
+
